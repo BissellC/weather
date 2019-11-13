d077a035bcaa5cf6befb8ec26a565c3f9785cff8
Charlie Bissell <bissell31@gmail.com>
2019-11-12 21:28:40 -0500

    basic functioning weather app

--- public/main.js
+++ public/main.js
@@ -1,7 +1,35 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let input
+let weatherData
+
+const checkInput = () => {
+  input = document.querySelector('input').value
+
+  if (isNaN(parseInt(input)) === false) {
+    input = 'zip=' + input
+  } else {
+    input = 'q=' + input
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const checkWeather = async () => {
+  checkInput()
+  const resp = await fetch(
+    'http://api.openweathermap.org/data/2.5/weather?' +
+      input +
+      '&APPID=dd457756f99158928d9b26f030f1e296&units=imperial'
+  )
+  console.log(resp)
+  weatherData = await resp.json()
+  console.log(weatherData)
+  displayWeather(weatherData)
+}
+
+const displayWeather = weatherData => {
+  document.querySelector('.temp').textContent =
+    Math.round(weatherData.main.temp) + '°F'
+  document.querySelector('.skies').textContent = weatherData.weather[0].main
+  document.querySelector('.humidity').textContent =
+    weatherData.main.humidity + '%'
+}
+
+document.querySelector('.search').addEventListener('click', checkWeather)
